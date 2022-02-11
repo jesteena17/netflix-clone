75b86d2169cf1114fb40100d56d207f9569fbf05
jesteena17 <jesteenababy17@gmail.com>
2022-02-11 19:29:12 +0530

    created header and body

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 
 import './App.css';
+import Banner from './components/banner/Banner';
+import Header from './components/header/Header';
+import Posts from './components/posts/Posts';
 
 function App() {
   return (
     <div className="App">
-     
+      <Header />
+      <Banner />
+      <Posts/>
+    
     </div>
   );
 }
---
+++ src/components/banner/Banner.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './banner.css';
+
+function Banner() {
+  return (
+    <div className="banner">
+          <div className="content">
+              <h1 className="title">Movie Name</h1>
+              <div className="banner-buttons">
+                  <button className="button">Play</button>
+                  <button className="button">My List</button>
+              </div>
+             
+              <h1 className='descr'>In publishing and graphic design, Lorem ipsum is a placeholder.
+                  In publishing and graphic design, Lorem ipsum is a placeholder </h1>
+                  
+      </div>
+      <div className="bottom">
+
+      </div>
+    </div>
+  )
+}
+
+export default Banner
